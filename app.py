5b491c2fde4b5ffaddf889b3ffc144010d4c1179
Hemantchaurasiya <hemantchaurasiya1122@gmail.com>
2021-07-24 06:53:22 -0700

    first commit

---
+++ app.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from flask import Flask,render_template,request,redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql.expression import desc
+from sqlalchemy.util.langhelpers import method_is_overridden
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+#database
+class Todo(db.Model):
+    sno = db.Column(db.Integer,primary_key = True)
+    title = db.Column(db.String(200),nullable=False)
+    desc = db.Column(db.String(500),nullable=False)
+    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __repr__(self) ->str:
+        return f"{self.sno} - {self.title}"
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method =='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo(title = title , desc = desc)
+        db.session.add(todo)
+        db.session.commit()
+    allTodo = Todo.query.all()
+    return render_template('index.html',allTodo=allTodo)
+
+@app.route('/show')
+def show():
+    allTodo = Todo.query.all()
+
+@app.route('/update/<int:sno>',methods=['GET','POST'])
+def update(sno):
+    if request.method=='POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+    todo = Todo.query.filter_by(sno=sno).first()
+    return render_template('update.html',todo=todo)
+
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+if __name__ == "__main__":
+    app.run(debug=True)
